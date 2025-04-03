8aab761bf2df51aa86403318f9630e8c4f2fc832
Glory42 <gorkemkaryol03@gmail.com>
2025-04-04 01:00:03 +0300

    Schedule daily USD/TRY rate updates and save to exchange.json

--- utils/cron.js
+++ utils/cron.js
@@ -1,13 +1,42 @@
 const cron = require('node-cron');
+const fs = require('fs');
+const path = require('path');
 const { getUsdTryRate } = require('../config/api');
+const exchangeJsonPath = path.join(__dirname, '../data/exchange.json');
 
-cron.schedule('0 9 * * *', async () => {
+async function updateExchangeRate () {
     try {
-        await getUsdTryRate();
-        console.log('Rate updated');
+        const apiResponse = await getUsdTryRate();
+        
+        if (!apiResponse || !apiResponse.rates || !apiResponse.rates.TRY) {
+            throw new Error('API response format is not as expected');
+        }
+
+        const today = new Date();
+        const formateDate = today.toISOString().split('T')[0];
+
+
+        const rate = parseFloat(apiResponse.rates.TRY);
+        const exchangeData = JSON.parse(fs.readFileSync(exchangeJsonPath, 'utf-8'));
+        const dailyIndex = exchangeData.daily.findIndex(entry => entry.date === formateDate);
+
+        if (dailyIndex !== -1) {
+            exchangeData.daily[dailyIndex].rate = rate;
+        } else {
+            exchangeData.daily.push({
+                date: formateDate,
+                rate: rate
+            });
+        };
+
+        fs.writeFileSync(exchangeJsonPath, JSON.stringify(exchangeData, null, 2), 'utf-8');
+
+        console.log(`Rate updated: ${formateDate}, rate: ${rate}`);
     } catch (error) {
-        console.error('Rate updated could not be made:', error);
+        console.error('Rate update could not be made:', error);
     }
-});
+};
+
+cron.schedule('0 9 * * *', updateExchangeRate);
 
 console.log('Cron function started');
