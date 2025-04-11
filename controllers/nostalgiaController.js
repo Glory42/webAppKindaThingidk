14a00e215cee4ce9199c7450afd96d079cf68920
Glory42 <gorkemkaryol03@gmail.com>
2025-04-11 16:52:44 +0300

    feat: implement product price comparison with historical exchange rates

--- controllers/nostalgiaController.js
+++ controllers/nostalgiaController.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 
-
 const productsPath = path.join(__dirname, '../data/products.json');
 const historicalPath = path.join(__dirname, '../data/historicalExchanges.json');
 const dailyPath = path.join(__dirname, '../data/dailyExchanges.json');
@@ -15,7 +14,7 @@ const compareHistoricalPrices = async (req, res) => {
         };
 
         const historicalData = JSON.parse(fs.readFileSync(historicalPath, 'utf-8'));
-        const dailylData = JSON.parse(fs.readFileSync(dailyPath, 'utf-8'));
+        const dailyData = JSON.parse(fs.readFileSync(dailyPath, 'utf-8'));
         const productData = JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
         
         const historicalRate = historicalData.historical.find(item => item.year === parseInt(year));
@@ -23,60 +22,58 @@ const compareHistoricalPrices = async (req, res) => {
             return res.status(404).json({ error: "Historical rate not found for the specified year" });
         };
 
-        const dailyRate = dailylData.daily[dailylData.daily.length -1].rate;
+        const dailyRate = dailyData.daily[dailyData.daily.length -1].rate;
 
         const product = productData.find(item => item.id === parseInt(productId));
         if (!product) {
             return res.status(404).json({ error: "Product not found" });
         };
 
-        let historicalPrice, currentPrice;
+        let historicalPrice;
 
         if (product.name === 'Iphone') {
             const historicalModel = product.models.find(model => model.year === parseInt(year));
-            const currentModel = product.models.find(model => model.year === new Date().getFullYear());
-            if (!historicalModel || !currentModel) {
+            if (!historicalModel) {
                 return res.status(404).json({ error: "Product data not found for the specified years" });
             };
 
             const historicalVersion = historicalModel.versions[0];
-            const currentVersion = currentModel.versions[0];
 
             historicalPrice = {
-                model: historicalPrice.model,
-                priceTRY: historicalVersion.price,
-                priceUSD: historicalVersion.price / historicalRate.rate,
+                model: historicalVersion.model,
+                priceTRY: historicalVersion.price * 1000,
+                priceUSD: parseFloat((historicalVersion.price * 1000 / historicalRate.rate).toFixed(2)),
             };
 
-            currentPrice = {
-                priceTRY: currentVersion.price,
-                priceUSD: currentVersion.price / currentRate
-            };
+             
         } else {
             const historicalPriceData = product.prices.find(p => p.year === parseInt(year));
-            const currentPriceData = product.prices.find(p => p.year == new Date().getFullYear());
 
-            if (!historicalPriceData || !currentPriceData) {
+            if (!historicalPriceData) {
                 return res.status(404).json({ error: "Product price not found for the specified years" });
             };
             
             historicalPrice = {
                 priceTRY: historicalPriceData.price,
-                priceUSD: historicalPriceData.price / historicalRate.rate
             };
             
-            currentPrice = {
-                priceTRY: currentPriceData.price,
-                priceUSD: currentPriceData.price / currentRate
-            };
             
             if (product.name.includes("Ekmek")) {
                 historicalPrice.gram = historicalPriceData.gram;
-                currentPrice.gram = currentPriceData.gram;
             };
         };
 
         res.json({
+            product: product.name,
+            category: product.category,
+            currentRate: dailyRate,
+            yearComparison: {
+                historical: {
+                    year: parseInt(year),
+                    exchangeRate: historicalRate.rate,
+                    ...historicalPrice
+                },
+            },
             
         })
     } catch (error) {
