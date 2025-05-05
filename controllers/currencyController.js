f0b67bfee0174c72b482efb6579b285ced1cda09
Glory42 <gorkemkaryol03@gmail.com>
2025-05-05 20:29:23 +0300

    Feat: update currencyController.js to get api and db data separated

--- controllers/currencyController.js
+++ controllers/currencyController.js
@@ -1,9 +1,18 @@
 const { getUsdTryRate } = require('../config/api');
+const { getRecentExchangeRates } = require('../services/exchangeRateService');
 
 const getLatestUsdTryRate = async (req, res) => {
     try {
-        const data = await getUsdTryRate();
-        res.json(data);
+        // api'dan gelen veriyi al
+        const apiData = await getUsdTryRate();
+
+        // DB'den gelen veriyi al
+        const dbData = await getRecentExchangeRates(7);
+
+        res.json({
+            current: apiData,
+            recent: dbData
+        });
     } catch (error) {
         res.status(500).json({ error: "Could not get rate change" });
     }
